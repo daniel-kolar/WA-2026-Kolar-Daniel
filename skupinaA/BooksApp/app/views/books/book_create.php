45a88e92e0c487070ffe3839551949ab9ce3ff10
Daniel Kolar <daniel.kolar@tul.cz>
2026-03-19 11:25:55 +0100

    Úkol 05 - PHP projekt - část 01 - uprava formuláře book_create

--- skupinaA/BooksApp/app/views/books/book_create.php
+++ skupinaA/BooksApp/app/views/books/book_create.php
@@ -14,29 +14,61 @@
         </div>
 
         <div>
-            <form action="">
+            <form action="../controllers/BookController.php" method="POST" enctype="multipart/form-data">
                 <div>
                     <div>
-                        <label for="title">Název knihy:</label>
+                        <label for="title">Název knihy*</label>
                         <input type="text" id="title" name="title" required>
                     </div>
 
                     <div>
-                        <label for="author">Autor:</label>
-                        <input type="text" id="author" name="author" required>
+                        <label for="author">Autor*</label>
+                        <input type="text" id="author" name="author" placeholder="Příjmení Jméno" required>
                     </div>
                     <div>
-                        <label for="year">Rok vydání:</label>
+                        <label for="isbn">ISBN*</label>
+                        <input type="text" id="isbn" name="isbn" required>
+                    </div>
+
+                    <div>
+                        <label for="year">Rok vydání*</label>
                         <input type="number" id="year" name="year" required>
                     </div>
+
                     <div>
-                        <label for="category">Kategorie:</label>
+                        <label for="category">Kategorie</label>
                         <input type="text" id="category" name="category">
-                    </div>  
+                    </div>
+
                     <div>
-                        <label for="subcategory">Podkategorie:</label>
+                        <label for="subcategory">Podkategorie</label>
                         <input type="text" id="subcategory" name="subcategory">
                     </div>
+
+                    <div>
+                        <label for="price">Cena knihy*</label>
+                        <input type="number" id="price" name="price" step="0.01" required>
+                    </div>
+
+                    <div>
+                        <label for="link">Odkaz</label>
+                        <input type="text" id="link" name="link">
+                    </div>
+
+                    <div>
+                        <label for="description">Popis knihy</label>
+                        <textarea id="description" name="description" rows="5"></textarea>
+                    </div>
+
+                    <div>
+                        <label>Obrázky (můžete nahrát více)</label>
+                        <label>
+                            <span>Klikni pro výběr souborů</span>
+                            <span>JPG / PNG / WebP – více souborů najednou</span>
+                            <input type="file" id="images" name="images[]" multiple accept="image/*" class="hidden">
+                        </label>
+                    </div>
+            
 
                     <div>
                         <button type="submit">Přidat knihu od DB</button>
