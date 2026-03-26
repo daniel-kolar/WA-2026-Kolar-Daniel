0a896d7b738d23b326b9309a69ba339efbb720b7
Daniel Kolar <daniel.kolar@tul.cz>
2026-03-26 11:14:05 +0100

    Úkol 08 - Routování - vytvoření základního routeningu

---
+++ skupinaA/BooksApp/public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// Pro účely výuky a ladění na lokálním serveru (např. XAMPP)
+// je vhodné zapnout kompletní zobrazení chyb, aby bylo možné snadno identifikovat a opravit problémy v kódu.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Načtení třídy routeru, která bude zpracovávat příchozí HTTP požadavky a směrovat je na správné kontrolery a akce.
+require_once '../core/App.php';
+
+// Inicializace aplikace, která vytvoří instanci třídy App a spustí router, aby mohl zpracovávat požadavky.
+$app = new App();
